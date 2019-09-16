e355601560e276f09de94055fd2a2c9097299a31
Lauren Auchter <lsauchter@gmail.com>
2019-09-16 18:53:35 -0400

    Update whitelist ability

--- src/app.js
+++ src/app.js
@@ -15,7 +15,19 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors({ origin: CLIENT_ORIGIN }))
+
+const whitelist = [CLIENT_ORIGIN, 'http://localhost:3000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions))
 
 app.use('/api/users', usersRouter)
 app.use('/api/inventory', inventoryRouter)
